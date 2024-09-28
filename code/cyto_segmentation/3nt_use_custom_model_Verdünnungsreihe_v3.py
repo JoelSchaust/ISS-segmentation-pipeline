82047a7f6822f22e562fc7152b6e29c76b3e1304
JoelSchaust <141397888+JoelSchaust@users.noreply.github.com>
2024-09-28 08:11:06 +0200

    Update 3nt_use_custom_model_Verdünnungsreihe_v3.py
    added explanations, changed description

--- code/cyto_segmentation/3nt_use_custom_model_Verdünnungsreihe_v3.py
+++ code/cyto_segmentation/3nt_use_custom_model_Verdünnungsreihe_v3.py
@@ -3,6 +3,7 @@ from glob import glob
 import os
 import argparse
 
+#to run this script, change the arguments according to the parameters you want to use, especially if your name scheme doesnt fit 
 parser = argparse.ArgumentParser(description='Use custom Cellpose model for cytoplasm segmentation')
 parser.add_argument('--ct', type=float, default=-6.0, help='cellprob threshold (-6.0 to 6.0')
 parser.add_argument('--ft', type=float, default=1.0, help='flowthreshold (0.0 to 1.0')
@@ -12,12 +13,11 @@ parser.add_argument('--rep', type=int, default=2, help='repetition (0-2')
 parser.add_argument('--rnd', type=int, default=2, help='round (01-02')
 parser.add_argument('--fov', type=int, default=15, help='field of view (0-9 usually)')
 parser.add_argument('--MOI', type=float, default=1, help='1.0 or 0.3')
-parser.add_argument('--model', type=str, default="/home/s361852/Schreibtisch/in-situ-seq-segmentation/code/models/3nt3chan_v1", help='custom model')
-parser.add_argument('--input', type=str, default="/home/s361852/Schreibtisch/in-situ-seq-segmentation/data/segmentation_3nt_3chan_non_mutant/Verdünnungsreihe_v3/input_images/", help='input path')
-parser.add_argument('--output', type=str, default="/home/s361852/Schreibtisch/in-situ-seq-segmentation/data/segmentation_3nt_3chan_non_mutant/Verdünnungsreihe_v3/output_images/", help='output path')
+parser.add_argument('--model', type=str, default="path/to/your/cytoplasm_model", help='custom model')
+parser.add_argument('--input', type=str, default="path/to/3channel/images/cytoplasm/", help='input path')
+parser.add_argument('--output', type=str, default="path/to/ouput/folder/", help='output path')
 args = parser.parse_args()
 
-#3nt3chan_rep{rep}_{MOI}MOI_{hpi}hpi_fov{fov}_6Inc_round{rnd}_ch0-2-4_s{s}.tiff
 rnd = args.rnd
 ct = args.ct
 s = args.s
@@ -30,8 +30,7 @@ model_path = args.model
 input_path = args.input
 output_path = args.output
 
-#3nt3chan_rep2_1MOI_6hpi_fov15_6Inc_round02_ch0-2-4_s5.tiff
-file = f"{input_path}3nt3chan_rep{rep}_{MOI}MOI_{hpi}hpi_fov{fov}_6Inc_round0{rnd}_ch0-2-4_s{s}.tiff"
+file = f"{input_path}3nt3chan_rep{rep}_{MOI}MOI_{hpi}hpi_fov{fov}_6Inc_round0{rnd}_ch0-2-4_s{s}.tiff" #specific to name format of your data
 flow_threshold = ft
 cellprob_threshold = ct
 model = models.CellposeModel(gpu=True, pretrained_model=model_path)
